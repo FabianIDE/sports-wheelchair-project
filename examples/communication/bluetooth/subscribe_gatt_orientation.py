476a0368f7c13f4defa7f5fc253540d6b02c694e
Weetzel <maxweetzel@gmail.com>
2019-03-11 15:30:52 +0100

    mac adress unique

--- examples/communication/bluetooth/subscribe_gatt_orientation.py
+++ examples/communication/bluetooth/subscribe_gatt_orientation.py
@@ -15,7 +15,7 @@ from dcd.entities.property_type import PropertyType
 load_dotenv()
 THING_ID = os.environ['THING_ID']
 THING_TOKEN = os.environ['THING_TOKEN']
-BLUETOOTH_DEVICE_MAC = os.environ['BLUETOOTH_DEVICE_MAC']
+BLUETOOTH_DEVICE_MAC = os.environ['BLUETOOTH_DEVICE_MAC_WHEEL']
 
 # UUID of the GATT characteristic to subscribe
 GATT_CHARACTERISTIC_ORIENTATION = "02118833-4455-6677-8899-AABBCCDDEEFF"
