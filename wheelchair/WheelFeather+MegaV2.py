0b4c589e7567450cc104d2b157cab5ae8cbdc74d
Weetzel <maxweetzel@gmail.com>
2019-03-18 22:24:43 +0100

    more comma changes, last one gave a encodeing error

--- wheelchair/WheelFeather+MegaV2.py
+++ wheelchair/WheelFeather+MegaV2.py
@@ -227,8 +227,8 @@ def start_HRM():
             #udate new readings to grafana
             my_property_HRM.update_values(intvalue)
             ser.write(str(intvalue).encode())
-            ser.write(",")
-            
+            ser.write(",".encode)
+
         #    ser.write(','.encode())
             print("HRM sent to arduino")
         except KeyboardInterrupt:
