61ab1461ec6b2436a3cc49ec1488643d6a1cdef3
FabianIDE <fabian.bosman@hotmail.com>
2019-04-01 11:55:21 +0200

    ADDED LABEL

--- wheelchair/Collect_and_labelV1.1.0.py
+++ wheelchair/Collect_and_labelV1.1.0.py
@@ -25,15 +25,22 @@ BLUETOOTH_DEVICE_MAC_WHEEL = os.environ['BLUETOOTH_DEVICE_MAC_WHEEL']
 GATT_CHARACTERISTIC_ORIENTATION = "02118833-4455-6677-8899-AABBCCDDEEFF" # UUID of the GATT characteristic to subscribe
 ADDRESS_TYPE = pygatt.BLEAddressType.random # Many devices, e.g. Fitbit, use random addressing, this is required to connect.
 
+# OPEN Serial
+def open_serial(): # Open a serial connection
+    # Start reading the serial port
+    return serial.Serial(
+        port=os.environ['SERIAL'],
+        baudrate=9600,
+        timeout=2)
 
+# BLE
 def find_or_create(property_name, property_type): # create property for BLE
     """Search a property by name, create it if not found, then return it."""
     if my_thing.find_property_by_name(property_name) is None:
         my_thing.create_property(name=property_name,
                                  property_type=property_type)
     return my_thing.find_property_by_name(property_name)
-
-def handle_orientation_data(handle, value_bytes): # print wheel values
+def handle_orientation_data(handle, value_bytes): # print wheel values, send to server
     """
     handle -- integer, characteristic read handle the data was received on
     value_bytes -- bytearray, the data returned in the notification
@@ -42,7 +49,6 @@ def handle_orientation_data(handle, value_bytes): # print wheel values
     values = [float(x) for x in value_bytes.decode('utf-8').split(",")]
     find_or_create("Right Sports Wheel",
                    PropertyType.THREE_DIMENSIONS).update_values(values)
-
 def discover_characteristic(device): # Provide UUID
     """List characteristics of a device"""
     for uuid in device.discover_characteristics().keys():
@@ -50,11 +56,9 @@ def discover_characteristic(device): # Provide UUID
             print("Read UUID" + str(uuid) + "   " + str(device.char_read(uuid)))
         except:
             print("Something wrong with " + str(uuid))
-
 def read_characteristic(device, characteristic_id): # return BLE characteristic
     """Read a characteristic"""
     return device.char_read(characteristic_id)
-
 def keyboard_interrupt_handler(signal_num, frame): #Provide interrupt
     """Make sure we close our program properly"""
     print("Exiting...".format(signal_num))
@@ -63,22 +67,91 @@ def keyboard_interrupt_handler(signal_num, frame): #Provide interrupt
     child.sendline("char-write-req 0x000f 0000")
     exit(0)
 
+# HRM
+def hexStrToInt(hexstr):
+    val = int(hexstr[3:5], 16) #
+    if ((val & 0x8000) == 0x8000): # treat signed 16bits
+        val = -((val ^ 0xffff) + 1)
+        val = int(val)
+    return val
+
+# LABEL
+def collect(class_index): #Collect data
+    # if we covered all classes, stop the program
+    if class_index >= len(CLASSES):
+        print("Data collection done.")
+        exit()
+
+    # Prompt the user to get ready and wait
+    print("Get ready to collect the MOVEMENT: " + CLASSES[class_index]
+          + " in " + str(DELAY_BETWEEN_MOVEMENT) + " seconds!")
+    time.sleep(DELAY_BETWEEN_MOVEMENT)
+
+    # Open the serial connection
+    print("Collecting data for MOVEMENT " + CLASSES[class_index])
+    ser = open_serial()
+
+    # Start reading serial port with the MOVEMENT index, start at sample 0.
+    sample = 0
+    while sample < MAX_SAMPLES:
+        if serial_to_property_values(class_index, ser):
+            sample += 1
+            print()
+    ser.close()
+    collect(class_index + 1)
+def serial_to_property_values(class_index, ser): #Add label to data
+    # Read one line
+    line_bytes = ser.readline()
+    # If the line is not empty
+    if len(line_bytes) > 0:
+        # Convert the bytes into string
+        line = line_bytes.decode('utf-8')
+        # Split the string using commas as separator, we get a list of strings
+        str_values = line.split(',')
+        # Remove the first id
+        # str_values.pop(0)
+        # Transform the array of string values into float values (numbers)
+        values = [float(x) for x in str_values]
+        values[4] =
+
+        # get the current time in milliseconds
+        current_ts_ms = int(round(time.time() * 1000))
+        # Update values of data and label properties (send them to the DCD Hub)
+        # With the same timestamp, so we can easily connect label and raw data later
+        prop_label.update_values([class_index], current_ts_ms)
+        prop_data.update_values(values, current_ts_ms)
+
+        return True
+    return False
+
+
+
 # Instantiate a thing with its credential, then read its properties from the DCD Hub
 my_thing = Thing(thing_id=THING_ID, token=THING_TOKEN)
 my_thing.read()
-
 print(my_thing.to_json())
 
-# Start a BLE adapter
-bleAdapter = pygatt.GATTToolBackend()
+bleAdapter = pygatt.GATTToolBackend() # Start a BLE adapter
 bleAdapter.start()
-
-# Use the BLE adapter to connect to our device
-left_wheel = bleAdapter.connect(BLUETOOTH_DEVICE_MAC_WHEEL, address_type=ADDRESS_TYPE)
-
-
-
-# Register our Keyboard handler to exit
-signal.signal(signal.SIGINT, keyboard_interrupt_handler)
+left_wheel = bleAdapter.connect(BLUETOOTH_DEVICE_MAC_WHEEL, address_type=ADDRESS_TYPE) # Use the BLE adapter to connect to our device
+signal.signal(signal.SIGINT, keyboard_interrupt_handler) # Register our Keyboard handler to exit
 
 #end BLE code
+
+
+# START COLLECTING
+
+# Instantiate a thing with its credential
+my_thing = Thing(thing_id=THING_ID, token=THING_TOKEN)
+
+# Read the details of our Thing from the DCD Hub to get property details
+my_thing.read()
+
+# Find label and data property by name
+prop_label = my_thing.find_property_by_name(LABEL_PROP_NAME)
+prop_data = my_thing.find_property_by_name(DATA_PROP_NAME)
+
+# Start collecting data for the first class
+collect(0)
+
+# END OF COLLECTING
